489bdc64e7aa2a8d53f6e96c9c0cc70d7f0ac7e1
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-21 14:51:30 +0300

    category crud

--- IlkMvcSayfam/Controllers/CategoryController.cs
+++ IlkMvcSayfam/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using IlkMvcSayfam.Models;
+﻿using System;
+using IlkMvcSayfam.Models;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -24,6 +25,39 @@ namespace IlkMvcSayfam.Controllers
             if (data == null) RedirectToAction("Index");
 
             return View(data);
+        }
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var db = new NorthwindEntities();
+            try
+            {
+                var category = db.Categories.Find(id.GetValueOrDefault());
+                if (category == null) return RedirectToAction("Index");
+
+                db.Categories.Remove(category);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return RedirectToAction("Index");
         }
     }
 }
