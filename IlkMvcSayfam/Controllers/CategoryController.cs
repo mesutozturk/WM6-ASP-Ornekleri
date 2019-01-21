01882ee938cf77f46b2bcf5ea7e3cdc8b32c6410
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-21 14:19:30 +0300

    category page

---
+++ IlkMvcSayfam/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using IlkMvcSayfam.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace IlkMvcSayfam.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var data = new NorthwindEntities()
+                .Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+            return View(data);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+
+            var data = new NorthwindEntities().Categories.Find(id.Value);
+            if (data == null) RedirectToAction("Index");
+
+            return View(data);
+        }
+    }
+}
