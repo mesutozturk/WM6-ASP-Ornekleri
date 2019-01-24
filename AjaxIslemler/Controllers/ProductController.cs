d5bc487cd6662a3619b03ec8db91b34c06de6dea
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-24 11:36:05 +0300

    product ctrl

---
+++ AjaxIslemler/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxIslemler.Models;
+using AjaxIslemler.Models.ViewModels;
+
+namespace AjaxIslemler.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllCategories()
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var data = db.Categories.Select(x => new CategoryViewModel()
+                {
+                    CategoryName = x.CategoryName,
+                    Description = x.Description,
+                    CategoryID = x.CategoryID,
+                    ProductCount = x.Products.Count
+                }).ToList();
+                return Json(new ResponseData()
+                {
+                    success = true,
+                    data = data
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    success = false,
+                    message = $"Bir hata olustu {ex.Message}"
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetAllProducts()
+        {
+            try
+            {
+                var db = new NorthwindEntities();
+                var data = db.Products.OrderBy(x => x.ProductName)
+                    .ToList()
+                    .Select(x => new ProductViewModel()
+                    {
+                        CategoryName = x.Category?.CategoryName,
+                        AddedDate = x.AddedDate,
+                        CategoryID = x.CategoryID,
+                        ProductName = x.ProductName,
+                        UnitsInStock = x.UnitsInStock,
+                        UnitPrice = x.UnitPrice,
+                        ProductID = x.ProductID,
+                        AddedDateFormatted = $"{x.AddedDate:g}",
+                        Discontinued = x.Discontinued,
+                        QuantityPerUnit = x.QuantityPerUnit,
+                        ReorderLevel = x.ReorderLevel,
+                        SupplierID = x.SupplierID,
+                        SupplierName = x.Supplier?.CompanyName,
+                        UnitPriceFormatted = $"{x.UnitPrice:c2}",
+                        UnitsOnOrder = x.UnitsOnOrder
+                    })
+                    .ToList();
+                return Json(new ResponseData()
+                {
+                    success = true,
+                    data = data
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    success = false,
+                    message = $"Bir hata olustu {ex.Message}"
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ AjaxIslemler/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AjaxIslemler.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string UnitPriceFormatted { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+        public DateTime AddedDate { get; set; }
+        public string AddedDateFormatted { get; set; }
+        public string CategoryName { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
