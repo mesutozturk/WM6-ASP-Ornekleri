c563a86bbac0e4aa06140cbf323611fd13eed7b3
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-25 14:55:56 +0300

    rapor google charts

---
+++ AjaxIslemler/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxIslemler.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
