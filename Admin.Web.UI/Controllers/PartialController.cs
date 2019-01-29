75f425b0e7ae45597e54abe854f82bd1de324c16
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-29 11:49:17 +0300

    template girdirme ok

---
+++ Admin.Web.UI/Controllers/PartialController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Web.UI.Controllers
+{
+    public class PartialController : Controller
+    {
+        public PartialViewResult DrawerPartial()
+        {
+            var data = new List<string>();
+            return PartialView("Partial/_DrawerPartial", data);
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView("Partial/_HeaderPartial");
+        }
+        public PartialViewResult ModalPartial()
+        {
+            return PartialView("Partial/_ModalPartial");
+        }
+    }
+}
---
+++ Admin.Web.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Admin.Web.UI.MvcApplication" Language="C#" %>
