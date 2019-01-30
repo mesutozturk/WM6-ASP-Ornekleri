5bdd790d9cedb6394e2b529ab911090a65adfae1
Mesut Öztürk <mesutoztrk@msn.com>
2019-01-30 09:39:51 +0300

    helpers ok

---
+++ Admin.BLL/Helpers/EntityHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace Admin.BLL.Helpers
+{
+    public static class EntityHelpers
+    {
+        public static string ValidationMessage(DbEntityValidationException ex)
+        {
+            var result = ex.EntityValidationErrors.Aggregate(string.Empty, (accumulator2, validationErrors) => validationErrors.ValidationErrors.Aggregate(accumulator2, (accumulator, validationError) => accumulator += $"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}\n"));
+            return result + "\n";
+        }
+    }
+}
---
+++ Admin.BLL/Helpers/StringHelpers.cs
@@ -0,0 +1,36 @@
+﻿namespace Admin.BLL.Helpers
+{
+    public static class StringHelpers
+    {
+        public static string UrlFormatConverter(string name)
+        {
+            string sonuc = name.ToLower();
+            sonuc = sonuc.Replace("'", "");
+            sonuc = sonuc.Replace(" ", "-");
+            sonuc = sonuc.Replace("<", "");
+            sonuc = sonuc.Replace(">", "");
+            sonuc = sonuc.Replace("&", "");
+            sonuc = sonuc.Replace("[", "");
+            sonuc = sonuc.Replace("!", "");
+            sonuc = sonuc.Replace("]", "");
+            sonuc = sonuc.Replace("ı", "i");
+            sonuc = sonuc.Replace("ö", "o");
+            sonuc = sonuc.Replace("ü", "u");
+            sonuc = sonuc.Replace("ş", "s");
+            sonuc = sonuc.Replace("ç", "c");
+            sonuc = sonuc.Replace("ğ", "g");
+            sonuc = sonuc.Replace("İ", "I");
+            sonuc = sonuc.Replace("Ö", "O");
+            sonuc = sonuc.Replace("Ü", "U");
+            sonuc = sonuc.Replace("Ş", "S");
+            sonuc = sonuc.Replace("Ç", "C");
+            sonuc = sonuc.Replace("Ğ", "G");
+            sonuc = sonuc.Replace("|", "");
+            sonuc = sonuc.Replace(".", "-");
+            sonuc = sonuc.Replace("?", "-");
+            sonuc = sonuc.Replace(";", "-");
+            sonuc = sonuc.Replace("#", "-sharp");
+
+            return sonuc;
+        }
+}
