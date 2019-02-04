53132c7025486ca841c0793ef544e62e00130eee
Mesut Öztürk <mesutoztrk@msn.com>
2019-02-04 09:03:57 +0300

    db

---
+++ Admin.DAL/Migrations/201902011343039_a.cs
@@ -0,0 +1,16 @@
+namespace Admin.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class a : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
