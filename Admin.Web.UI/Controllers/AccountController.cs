7f8365a4aadf3d79f97fec1ad9bc2dd2d58cc5f8
Mesut Öztürk <mesutoztrk@msn.com>
2019-02-04 13:47:01 +0300

    register ok

--- Admin.Web.UI/Controllers/AccountController.cs
+++ Admin.Web.UI/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Admin.BLL.Identity;
+using Admin.Models.IdentityModels;
+using Admin.Models.ViewModels;
+using static Admin.BLL.Identity.MembershipTools;
 
 namespace Admin.Web.UI.Controllers
 {
@@ -12,6 +17,75 @@ namespace Admin.Web.UI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterLoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+            try
+            {
+                var userStore = NewUserStore();
+                var userManager = NewUserManager();
+
+                var rm = model.RegisterViewModel;
+
+                var user = await userManager.FindByNameAsync(rm.UserName);
+                if (user != null)
+                {
+                    ModelState.AddModelError("UserName", "Bu kullanıcı adı daha önceden alınmıştır");
+                    return View("Index", model);
+                }
+
+                var newUser = new User()
+                {
+                    UserName = rm.UserName,
+                    Email = rm.Email,
+                    Name = rm.Name,
+                    Surname = rm.Surname
+                };
+                var result = await userManager.CreateAsync(newUser, rm.Password);
+                if (result.Succeeded)
+                {
+                    if (userStore.Users.Count() == 1)
+                    {
+                        await userManager.AddToRoleAsync(newUser.Id, "Admin");
+                    }
+                    else
+                    {
+                        await userManager.AddToRoleAsync(newUser.Id, "User");
+                    }
+                    //todo kullanıcıya mail gönderilsin
+                }
+                else
+                {
+                    var err = "";
+                    foreach (var resultError in result.Errors)
+                    {
+                        err += resultError + " ";
+                    }
+                    ModelState.AddModelError("", err);
+                    return View("Index", model);
+                }
+
+                TempData["Message"] = "Kaydınız alınlıştır. Lütfen giriş yapınız";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "Index",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
