bbb05ecef4094b2ebe7b1dfb41ce8d90ad4338f1
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-02-06 13:06:44 +0300

    tekrar

--- Admin.Models/ViewModels/ProfilePasswordViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Admin.Models.ViewModels
-{
-    public class ProfilePasswordViewModel
-    {
-        public UserProfileViewModel UserProfileViewModel { get; set; }
-        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
-    }
-}
