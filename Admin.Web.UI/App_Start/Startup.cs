13a5868662a265f82a76b52577ecb29e39755637
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-02-08 09:41:52 +0300

    facebook login

--- Admin.Web.UI/App_Start/Startup.cs
+++ Admin.Web.UI/App_Start/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Web;
-using Admin.BLL.Helpers;
+﻿using Admin.BLL.Helpers;
 using Admin.BLL.Identity;
 using Admin.Models.IdentityModels;
 using Microsoft.AspNet.Identity;
@@ -9,8 +6,11 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
 using Owin;
+using System;
+using System.Web;
 
 [assembly: OwinStartup(typeof(Admin.Web.UI.App_Start.Startup))]
 
@@ -29,6 +29,72 @@ namespace Admin.Web.UI.App_Start
 
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
             ConfigureGoogle(app);
+            ConfigureFacebook(app);
+        }
+
+        private void ConfigureFacebook(IAppBuilder app)
+        {
+            const string appId = "345283286312425";
+            const string appSecret = "447e03a0096467eb0b60483c415168d4";
+            var options = new FacebookAuthenticationOptions
+            {
+                AppId = appId,
+                AppSecret = appSecret
+            };
+            options.Scope.Add("email");
+            options.Provider = new FacebookAuthenticationProvider()
+            { 
+                OnAuthenticated = async context =>
+                {
+                    var userDetail = context.User;
+
+                    string id = (dynamic)context.Id;
+
+                    string email = (dynamic)context.Email;
+                    var userStore = MembershipTools.NewUserStore();
+                    var userManager = new UserManager<User>(userStore);
+                    var currentUser = userManager.FindByEmail(email);
+                    if (currentUser != null)
+                    {
+                        var serializedUser = context.User;
+                        try
+                        {
+                            currentUser.EmailConfirmed = true;
+                            currentUser.Name = ((dynamic)context.User).first_name.Value;
+                            currentUser.Surname = ((dynamic)context.User).last_name.Value;
+                            //currentUser.RegisterDate = DateTime.Now;
+                            var avatar = @"https://graph.facebook.com/" + id + "/picture?type=large";
+
+                            var loginInfo = HttpContext.Current.GetOwinContext().Authentication.GetExternalLoginInfo();
+                            if (loginInfo != null)
+                            {
+                                var result = userManager.AddLogin(currentUser.Id, loginInfo.Login);
+                                Console.WriteLine("test");
+                            }
+                            await userStore.UpdateAsync(currentUser);
+                            await userStore.Context.SaveChangesAsync();
+                        }
+                        catch (Exception ex)
+                        {
+                            string x = ex.StackTrace.ToString();
+                        }
+                    }
+                    else
+                    {
+                        var user = new User()
+                        {
+                            Email = email,
+                            Name = ((dynamic) context.User).first_name.Value,
+                            Surname = ((dynamic) context.User).last_name.Value,
+                            UserName = email.Substring(0, email.IndexOf('@'))
+                        };
+                        userManager.Create( user,StringHelpers.GetCode().Substring(0, 6));
+                        var loginInfo = HttpContext.Current.GetOwinContext().Authentication.GetExternalLoginInfo();
+                        var result = userManager.AddLogin(user.Id, loginInfo.Login);
+                    }
+                }
+            };
+            app.UseFacebookAuthentication(options);
         }
 
         private void ConfigureGoogle(IAppBuilder app)
