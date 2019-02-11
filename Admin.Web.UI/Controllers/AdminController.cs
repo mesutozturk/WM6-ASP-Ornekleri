cc3a56c9f71fa6d6c54dfdb7992a88d437707046
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-02-11 11:24:30 +0300

    parola sıfırlama ok

--- Admin.Web.UI/Controllers/AdminController.cs
+++ Admin.Web.UI/Controllers/AdminController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Mvc;
-using Admin.BLL.Helpers;
-using Admin.BLL.Identity;
+﻿using Admin.BLL.Helpers;
 using Admin.BLL.Services.Senders;
 using Admin.Models.Models;
 using Microsoft.AspNet.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using static Admin.BLL.Identity.MembershipTools;
 
 namespace Admin.Web.UI.Controllers
@@ -58,6 +57,48 @@ namespace Admin.Web.UI.Controllers
                 return Json(new ResponseData()
                 {
                     message = "Kullanıcıya yeni aktivasyon maili gönderildi",
+                    success = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Bir hata oluştu: {ex.Message}",
+                    success = false
+                });
+            }
+        }
+        [HttpPost]
+        public async Task<JsonResult> SendPassword(string id)
+        {
+            try
+            {
+                var userStore = NewUserStore();
+                var user = await userStore.FindByIdAsync(id);
+                if (user == null)
+                {
+                    return Json(new ResponseData()
+                    {
+                        message = "Kullanıcı bulunamadı",
+                        success = false
+                    });
+                }
+
+                var newPassword = StringHelpers.GetCode().Substring(0, 6);
+                await userStore.SetPasswordHashAsync(user, NewUserManager().PasswordHasher.HashPassword(newPassword));
+                await userStore.UpdateAsync(user);
+                userStore.Context.SaveChanges();
+
+                string SiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                 (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+                var emailService = new EmailService();
+                var body = $"Merhaba <b>{user.Name} {user.Surname}</b><br>Hesabınızın parolası sıfırlanmıştır<br> Yeni parolanız: <b>{newPassword}</b> <p>Yukarıdaki parolayı kullanarak sistemize giriş yapabilirsiniz.</p>";
+                emailService.Send(new IdentityMessage() { Body = body, Subject = $"{user.UserName} Şifre Kurtarma" }, user.Email);
+
+                return Json(new ResponseData()
+                {
+                    message = "Şifre sıfırlama maili gönderilmiştir",
                     success = true
                 });
             }
