7d7b0c43102cbcaf264ca944c15a4daaf4d912e3
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-02-13 10:02:54 +0300

    area kullanımı

---
+++ AreaKullanimi/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AreaKullanimi.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Admin";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "AreaKullanimi.Areas.Admin.Controllers" }
+            );
+        }
+    }
+}
