d9c68ed4e5e8eb346f8b89e8abdab6e108d83249
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-02-28 12:00:53 +0300

    sepet + bootstrap4

--- WebApiAngularJs/Scripts/app.js
+++ WebApiAngularJs/Scripts/app.js
@@ -4,10 +4,16 @@ var app = angular.module("myApp", []);
 
 app.controller("ProductCtrl", function ($scope) {
     $scope.urunler = [];
+    $scope.sepetList = [];
+    $scope.ekleMi = false;
+    $scope.sepetToplam = 0;
 
     function init() {
         var data = JSON.parse(localStorage.getItem("urunler"));
         $scope.urunler = data === null ? [] : data;
+        data = JSON.parse(localStorage.getItem("sepet"));
+        $scope.sepetList = data === null ? [] : data;
+        sepetHesapla();
     }
     $scope.ekle = function () {
         $scope.urunler.push({
@@ -21,7 +27,7 @@ app.controller("ProductCtrl", function ($scope) {
         localStorage.setItem("urunler", JSON.stringify($scope.urunler));
     };
 
-    $scope.sil = function(id) {
+    $scope.sil = function (id) {
         for (var i = 0; i < $scope.urunler.length; i++) {
             var data = $scope.urunler[i];
             if (id === data.id) {
@@ -30,6 +36,40 @@ app.controller("ProductCtrl", function ($scope) {
             }
         }
         localStorage.setItem("urunler", JSON.stringify($scope.urunler));
+    };
+
+    $scope.sepeteekle = function (urun) {
+        var varMi = false;
+        for (var i = 0; i < $scope.sepetList.length; i++) {
+            var data = $scope.sepetList[i];
+            if (data.id === urun.id) {
+                varMi = true;
+                data.adet++;
+                break;
+            }
+        }
+        if (!varMi) {
+            urun.adet = 1;
+            $scope.sepetList.push(urun);
+        }
+        localStorage.setItem("sepet", JSON.stringify($scope.sepetList));
+        sepetHesapla();
+    };
+
+    function sepetHesapla() {
+        $scope.sepetToplam = 0;
+        for (var i = 0; i < $scope.sepetList.length; i++) {
+            var data = $scope.sepetList[i];
+            $scope.sepetToplam += data.adet * data.fiyat;
+        }
+    }
+
+    $scope.cikart = function (urun) {
+        var index = $scope.sepetList.indexOf(urun);
+        if (index > -1)
+            $scope.sepetList.splice(index, 1);
+        localStorage.setItem("sepet", JSON.stringify($scope.sepetList));
+        sepetHesapla();
     };
 
     function guid() {
