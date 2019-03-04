00f0c5cac83066e0169fb23c86e1f5e0a50d7dc8
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2019-03-04 11:30:11 +0300

    signalR ok

---
+++ SignalRApp/App_Start/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRApp.App_Start.Startup))]
+
+namespace SignalRApp.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRApp/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+
+        public void HerkeseGonder(string gonderen, string mesaj)
+        {
+            Clients.All.herkeseGonder(gonderen, mesaj, $"{DateTime.Now:g}");
+        }
+    }
+}
